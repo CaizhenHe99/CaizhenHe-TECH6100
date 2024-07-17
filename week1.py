a6d1e69d2c079aa56ff7e6c276061d1916ce6ad7
CaizhenHe99 <373959521@qq.com>
2024-07-17 21:00:10 +0800

    Tech6100_week1

---
+++ week1.py
@@ -0,0 +1,12 @@
+#integer, float, string
+#keywords: print, for, if , elif, else, each, case
+#variables: are space to store values: that is a name of the memory space
+# primary/Secondary - RAM(SRAM/DRAM)/ROM(cadue)
+
+first_value:int = 5
+second_value = 6
+print("%s is the first value and %s is the second value assigned"%(first_value, second_value))
+print(f"{first_value} is the first value, {second_value} is the second value")
+print("The first value is ", first_value, "second value is ", second_value)
+print("The first value" + "second value combined")
+print(type(first_value))
